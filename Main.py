7c514c8579e1a080245e226f543b8132424b55de
JoseDavidVindas <jose.chavarria34@ulatina.net>
2024-04-26 14:16:50 -0600

    Se sube el proyecto

---
+++ Main.py
@@ -0,0 +1,15 @@
+
+def main():
+    x = 10
+    y = 5
+
+    print("Operadores de comparación:")
+    print(f"x ({x}) es igual a y ({y})?", x == y)
+    print(f"x ({x}) es diferente de y ({y})?", x != y)
+    print(f"x ({x}) es mayor que y ({y})?", x > y)
+    print(f"x ({x}) es menor que y ({y})?", x < y)
+    print(f"x ({x}) es mayor o igual que y ({y})?", x >= y)
+    print(f"x ({x}) es menor o igual que y ({y})?", x <= y)
+
+if __name__ == "__main__":
+    main()
